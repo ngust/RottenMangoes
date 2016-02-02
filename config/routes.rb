33a89ff2d3defdf87e1ee248638db01cf9849a38
Nicholas Gust <nicholasgust@gmail.com>
2016-02-01 19:15:10 -0800

    added login, and functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 RottenMangoes::Application.routes.draw do
-  get "users/new"
-  get "users/create"
+  
+  get "sessions/new"
+  get "sessions/create"
   resources :movies
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
+
 end
