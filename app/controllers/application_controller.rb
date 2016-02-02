07372d20fa0d089c52a8146f855508f1628f8229
Nicholas Gust <nicholasgust@gmail.com>
2016-02-01 19:29:12 -0800

    added flash notices on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      redirect_to movies_path, notice: "#{@movie.title} was submitted successfully!"
+    else
+      render :new
+    end
+  end
 end
