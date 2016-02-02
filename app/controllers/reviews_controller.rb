efe1cf0abcd95de63a8a6db5b7137342f05e5547
Nicholas Gust <nicholasgust@gmail.com>
2016-02-02 13:41:18 -0800

    Added before filter to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,24 +1,29 @@
 class ReviewsController < ApplicationController
+  
+  before_filter :load_movie
+
   def new
-  @movie = Movie.find(params[:movie_id])
-  @review = @movie.reviews.build
-end
-
-def create
-  @movie = Movie.find(params[:movie_id])
-  @review = @movie.reviews.build(review_params)
-  @review.user_id = current_user.id
-
-  if @review.save
-    redirect_to @movie, notice: "Review created successfully"
-  else
-    render :new
+    @review = @movie.reviews.build
   end
-end
 
-protected
+  def create
+    @review = @movie.reviews.build(review_params)
+    @review.user_id = current_user.id
 
-def review_params
-  params.require(:review).permit(:text, :rating_out_of_ten)
-end
+    if @review.save
+      redirect_to @movie, notice: "Review created successfully"
+    else
+      render :new
+    end
+  end
+
+  protected
+
+  def load_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+      
+  def review_params
+    params.require(:review).permit(:text, :rating_out_of_ten)
+  end
 end
