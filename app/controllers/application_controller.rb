2da05fb4123857300b1a208e0d8bad3e38178b62
Nicholas Gust <nicholasgust@gmail.com>
2016-02-01 20:00:44 -0800

    added reviews, realtions and validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def create
-    @movie = Movie.new(movie_params)
-
-    if @movie.save
-      redirect_to movies_path, notice: "#{@movie.title} was submitted successfully!"
-    else
-      render :new
-    end
-  end
-
   protected
 
     def current_user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,13 @@
 class User < ActiveRecord::Base
-   has_secure_password
- 
-   validates :email,
-     presence: true
- 
-   validates :firstname,
-     presence: true
- 
-   validates :lastname,
-     presence: true
- 
-   validates :password,
-     length: { in: 6..20 }, on: :create
+  has_many :reviews
+
+  has_secure_password
+  validates :email,
+    presence: true
+  validates :firstname,
+    presence: true
+  validates :lastname,
+    presence: true
+  validates :password,
+    length: { in: 6..20 }, on: :create
 end
