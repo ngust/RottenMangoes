4337b75e127cb16e28fb3ed5f50e01dccef93a65
Nicholas Gust <nicholasgust@gmail.com>
2016-02-04 20:50:46 -0800

    added security for login, admin and edit functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def restrict_admin_access
+    if !current_user.admin?
+        flash[:alert] = "You are not admin, Go home."
+        redirect_to new_session_path
+    end
+  end
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
