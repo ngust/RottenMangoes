512476959640e01dbdbd6063894797005fc70866
Nicholas Gust <nicholasgust@gmail.com>
2016-03-12 11:22:35 -0800

    added all movies to movies.yml

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160203213634) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "movies", force: true do |t|
     t.string   "title"
     t.string   "director"
@@ -34,8 +37,8 @@ ActiveRecord::Schema.define(version: 20160203213634) do
     t.datetime "updated_at"
   end
 
-  add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id"
-  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id"
+  add_index "reviews", ["movie_id"], name: "index_reviews_on_movie_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email"
